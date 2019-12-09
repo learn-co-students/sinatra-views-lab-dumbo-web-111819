75cb68bc79f9fa80c1bc270c29ffa47fd3552a35
MohammedQasim <mqshaikh8@gmail.com>
2019-12-09 15:59:43 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	resp = Rack::Response.new
+	get '/hello' do
+		erb :hello
 	end
-
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+	get '/date' do
+		
+		erb :date
+	end
 
 end
